ade0b33c9e9769953f7920912e9e2f94db2b9823
Matías Halles <matias@zoup.cl>
2012-06-27 19:25:11 -0400

    ADD: Content Tag Meta Tag and Favicon

--- header.php
+++ header.php
@@ -3,6 +3,9 @@
 <head>
 	<title><?php bloginfo('name'); ?> » <?php echo (is_home()||is_front_page())?get_bloginfo('description'):wp_title('', false); ?></title>
 	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="content-language" content="<?php bloginfo('language'); ?>">
+	<link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
