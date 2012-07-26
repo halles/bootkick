5018d1a2f8db0cc67397b9a43a803f5d7a54f4ef
Matías Halles <matias@zoup.cl>
2012-07-25 22:51:04 -0400

    ADD: Breadcrumb function using Breadcrumb NavXT if available

---
+++ loop-post-breadcrumb.php
@@ -0,0 +1,3 @@
+<div class="breadcrumbs navxt">
+<?php if(function_exists('bcn_display')){ bcn_display(); }?>
+</div>
--- loop-post.php
+++ loop-post.php
@@ -1,5 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			
+	
+	<?php echo get_template_part('loop', 'post-breadcrumb'); ?>		
+	
 	<?php echo get_template_part('loop', 'post-header'); ?>
 	
 	<?php echo get_template_part('loop', 'post-content'); ?>
